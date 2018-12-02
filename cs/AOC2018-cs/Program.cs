3a31eb90f0c65f23a263f41d554548ab981ec4a6
Nathan <ntg9vz@virginia.edu>
2018-12-02 16:16:06 -0500

    initial commit

---
+++ cs/AOC2018-cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOC2018_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+        }
+    }
+}
